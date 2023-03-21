fe538d859298a80aaffb87814f93aa65bd9b508e
Gauss12345 <128465982+Gauss12345@users.noreply.github.com>
2023-03-21 17:08:15 +0800

    Add files via upload

---
+++ 1-3.cpp
@@ -0,0 +1,20 @@
+// 输入a,b的值，输出较大者
+#include <stdio.h>
+int main()
+{
+	int max(int x,int y);
+	int a,b,c;
+	scanf("%d,%d",&a,&b); //gyuygg
+	gg
+	c=max(a,b);
+	printf("max=%d\n",c);
+	return 0;
+}
+
+int max(int x,int y)
+{
+	int z;
+	if (x>y) z=x;
+	else z=y;
+	return(z);
+}
